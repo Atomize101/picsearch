ad43ff1d5b2aa406a1995e1e49043eebb540d860
Christopher Patrick <cpatrick@nabancard.com>
2019-01-31 10:40:05 -0500

    New Changes

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-
-
 class SearchBar extends React.Component {
+    onInputChange(e) {
+        console.log(e.target.value);
+    }
+    
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"></input>
+                        <input type="text" onChange={this.onInputChange}></input>
                     </div>
                 </form>
             </div>
